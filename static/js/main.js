30af7094935d0a5e7ee7022d3a615f41b2ee76fb
steven <steven@SFRAME-L.cinci.rr.com>
2023-07-20 17:25:19 -0500

    working on iframe loading sequence.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
 console.log("main.js")
+
+const iframes = document.querySelectorAll("iframe")
+
+for (iframe of iframes) {
+    iframe.onload = function() {
+        this.classList.add("loaded")
+    }
+}
