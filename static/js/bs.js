3776a9ed89aad8035c94e132d93bf94635a54a7b
steven <schaefsteven@gmail.com>
2023-07-26 14:28:29 -0500

    reduced js

---
+++ static/js/bs.js
@@ -0,0 +1,14 @@
+// Bootstrap imports
+// import '/node_modules/bootstrap/js/dist/alert.js';
+// import '/node_modules/bootstrap/js/dist/button.js';
+// import '/node_modules/bootstrap/js/dist/carousel.js';
+import '/node_modules/bootstrap/js/src/collapse.js';
+// import '/node_modules/bootstrap/js/dist/dropdown.js';
+// import '/node_modules/bootstrap/js/dist/modal.js';
+// import '/node_modules/bootstrap/js/dist/offcanvas.js';
+// import '/node_modules/bootstrap/js/dist/popover.js';
+// import '/node_modules/bootstrap/js/dist/scrollspy.js';
+// import '/node_modules/bootstrap/js/dist/tab.js';
+// import '/node_modules/bootstrap/js/dist/toast.js';
+// import '/node_modules/bootstrap/js/dist/tooltip.js';
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,3 @@
-// Bootstrap imports
-// import '/node_modules/bootstrap/js/dist/alert.js';
-// import '/node_modules/bootstrap/js/dist/button.js';
-// import '/node_modules/bootstrap/js/dist/carousel.js';
-import '/node_modules/bootstrap/js/dist/collapse.js';
-// import '/node_modules/bootstrap/js/dist/dropdown.js';
-// import '/node_modules/bootstrap/js/dist/modal.js';
-// import '/node_modules/bootstrap/js/dist/offcanvas.js';
-// import '/node_modules/bootstrap/js/dist/popover.js';
-// import '/node_modules/bootstrap/js/dist/scrollspy.js';
-// import '/node_modules/bootstrap/js/dist/tab.js';
-// import '/node_modules/bootstrap/js/dist/toast.js';
-// import '/node_modules/bootstrap/js/dist/tooltip.js';
-
 // fade-in for iframes to prevent white flash
 const iframes = document.querySelectorAll("iframe")
 
