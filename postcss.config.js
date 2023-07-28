f894461b9ba450d2d6857fb3045c4b0f695d92a1
steven <schaefsteven@gmail.com>
2023-07-28 14:17:47 -0500

    trying to get postprocess working, wip

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,14 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [ './hugo_stats.json' ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
 });
-const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  plugins: [
-    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
-  ],
-};
+     plugins: [
+         ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
+     ]
+ };
 
