9b966378b2e17baac6cbd2a9affdf1a0faec2cc8
steven <schaefsteven@gmail.com>
2023-08-01 15:04:00 -0500

    updated iframes loading animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,8 @@
 // fade-in for iframes to prevent white flash
 const iframes = document.querySelectorAll("iframe")
 
-for (iframe of iframes) {
-    if (iframe.complete) {
+window.onload = function() {
+    for (iframe of iframes) {
         iframe.classList.add("loaded")
     }
-    else {
-        iframe.onload = function() {
-            this.classList.add("loaded")
-        }
-    }
 }
-
