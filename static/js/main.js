00ebf5a42139fc14f53dd87883382c3746fc6ff4
steven <steven@SFRAME-L.cinci.rr.com>
2023-08-19 18:40:54 -0500

    clipboard functionality working, still need to add animation for confirmation.

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,12 @@ window.onload = function() {
         iframe.classList.add("loaded")
     }
 }
+
+const copyButtons = document.querySelectorAll(".copy")
+
+copyButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const content = button.parentNode.querySelector(".copy-target").innerText
+        navigator.clipboard.writeText(content)
+    })
+})
