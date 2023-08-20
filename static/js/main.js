7a81f0ffbce62bdd2262085446cbdb157554309c
steven <steven@SFRAME-L.cinci.rr.com>
2023-08-19 19:22:41 -0500

    added animation to clipboard icons

--- static/js/main.js
+++ static/js/main.js
@@ -12,6 +12,15 @@ const copyButtons = document.querySelectorAll(".copy")
 copyButtons.forEach((button) => {
     button.addEventListener("click", () => {
         const content = button.parentNode.querySelector(".copy-target").innerText
+        const icon = button.querySelector("i")
         navigator.clipboard.writeText(content)
+        button.classList.add("copy-success")
+        icon.classList.remove("bi-clipboard2")
+        icon.classList.add("bi-clipboard2-check-fill")
+        setTimeout(() => {
+        button.classList.remove("copy-success")
+        icon.classList.remove("bi-clipboard2-check-fill")
+        icon.classList.add("bi-clipboard2")
+        }, 3000)
     })
 })
